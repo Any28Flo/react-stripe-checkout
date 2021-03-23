5868c952c23ffe53901c967d6efa1680e744d949
Daedra <foglzerika@gmail.com>
2021-03-23 07:58:09 -0600

    feat | Create app,  adding and configure firebase

---
+++ src/firebase-config.js
@@ -0,0 +1,9 @@
+export default {
+      apiKey: "AIzaSyAMah0YdlvH5VLaxczkM9Rx5FZIhBQbk0Y",
+      authDomain: "eribooks-5b535.firebaseapp.com",
+      projectId: "eribooks-5b535",
+      storageBucket: "eribooks-5b535.appspot.com",
+      messagingSenderId: "214238639109",
+      appId: "1:214238639109:web:c44fd69b0ca449a58e186c",
+      measurementId: "G-5N9N9WYN17"
+}
